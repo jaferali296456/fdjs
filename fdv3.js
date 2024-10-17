20280064bf0356da221a5c95ad921b8897249f66
jaferali296456 <jaferali296456@gmail.com>
2024-10-17 22:08:00 +0500

    Create fdv3.js

---
+++ fdv3.js
@@ -0,0 +1,24 @@
+function replaceShortcodes() {
+    const shortcodes = {
+        fd: { name: '720p', regex: /\[fd id='(.*?)'\]/g, url: 'https://www.fastdrama.icu/p/' },
+        egt: { name: 'GDFLIX', regex: /\[egf id='(.*?)'\]/g, url: 'https://new7.gdtot.dad/file/' },
+        egf: { name: 'GDTOT', regex: /\[egt id='(.*?)'\]/g, url: 'https://new5.gdflix.cfd/file/' },
+    };
+
+    const contentContainers = document.querySelectorAll('.download-links');
+    contentContainers.forEach(contentContainer => {
+        let content = contentContainer.innerHTML;
+        for (const key in shortcodes) {
+            if (shortcodes.hasOwnProperty(key)) {
+                content = content.replace(shortcodes[key].regex, function(match, id) {
+                    return `<a href='${shortcodes[key].url}${id}' target='_blank'>
+                                ${shortcodes[key].name}
+                            </a>`;
+                });
+            }
+        }
+        contentContainer.innerHTML = content;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', replaceShortcodes);
