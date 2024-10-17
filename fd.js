89b84284148e5b64b193d249448d23373227f842
jaferali296456 <jaferali296456@gmail.com>
2024-10-17 19:45:23 +0500

    Create fd.js

---
+++ fd.js
@@ -0,0 +1,22 @@
+function replaceShortcodes() {
+    const shortcodes = {
+        fd: { name: '720p', regex: /\[fd id='(.*?)'\]/g, url: 'https://www.fastdrama.icu/' },
+    };
+
+    const contentContainers = document.querySelectorAll('.download-links');
+    contentContainers.forEach(contentContainer => {
+        let content = contentContainer.innerHTML;
+        for (const key in shortcodes) {
+            if (shortcodes.hasOwnProperty(key)) {
+                content = content.replace(shortcodes[key].regex, function(match, id) {
+                    return `<a href='${shortcodes[key].url}${id}' target='_blank'>
+                                ${shortcodes[key].name}
+                            </a>`;
+                });
+            }
+        }
+        contentContainer.innerHTML = content;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', replaceShortcodes);
